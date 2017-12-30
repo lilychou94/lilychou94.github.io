43d284ee58d51ab2f2bc95353db6f95ca2e4737a
zhouliwei <lily_chou@zhouliweideMacBook-Pro.local>
2017-12-30 15:40:17 +0800

    Revert "tiral4"
    This reverts commit 34e9465534ea1f7b8cfb4e372cec0bbb0dbb8043.

--- README.txt
+++ README.txt
@@ -1,11 +1,22 @@
+<<<<<<< HEAD
 Read Only by HTML5 UP
+=======
+Striped by HTML5 UP
+>>>>>>> parent of 34e9465... tiral4
 html5up.net | @ajlkn
 Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 
 
+<<<<<<< HEAD
 Just a super simple single-page responsive template built for personal sites and portfolios
 (although it'd definitely work for other stuff too). Includes a contact form, pre-styled
 elements, and Sass sources.
+=======
+And here's a new one: Striped, which features a clean, minimalistic design, styling for
+all basic page elements (including blockquotes, tables and lists), a repositionable
+sidebar (left or right), and HTML5/CSS3 code designed for quick and easy customization
+(see code comments for details).
+>>>>>>> parent of 34e9465... tiral4
 
 Demo images* courtesy of Unsplash, a radtastic collection of CC0 (public domain) images
 you can use for pretty much whatever.
@@ -17,8 +28,11 @@ Feedback, bug reports, and comments are not only welcome, but strongly encourage
 AJ
 aj@lkn.io | @ajlkn
 
+<<<<<<< HEAD
 PS: Not sure how to get that contact form working? Give formspree.io a try (it's awesome).
 
+=======
+>>>>>>> parent of 34e9465... tiral4
 
 Credits:
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,9 @@
 /*
+<<<<<<< HEAD
 	Read Only by HTML5 UP
+=======
+	Striped by HTML5 UP
+>>>>>>> parent of 34e9465... tiral4
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
@@ -7,23 +11,45 @@
 (function($) {
 
 	skel.breakpoints({
+<<<<<<< HEAD
 		xlarge: '(max-width: 1680px)',
 		large: '(max-width: 1280px)',
 		medium: '(max-width: 1024px)',
 		small: '(max-width: 736px)',
 		xsmall: '(max-width: 480px)'
+=======
+		desktop: '(min-width: 737px)',
+		wide: '(min-width: 1201px)',
+		narrow: '(min-width: 737px) and (max-width: 1200px)',
+		narrower: '(min-width: 737px) and (max-width: 1000px)',
+		mobile: '(max-width: 736px)'
+>>>>>>> parent of 34e9465... tiral4
 	});
 
 	$(function() {
 
+<<<<<<< HEAD
 		var $body = $('body'),
 			$header = $('#header'),
 			$nav = $('#nav'), $nav_a = $nav.find('a'),
 			$wrapper = $('#wrapper');
+=======
+		var	$window = $(window),
+			$body = $('body'),
+			$document = $(document);
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				$body.removeClass('is-loading');
+			});
+>>>>>>> parent of 34e9465... tiral4
 
 		// Fix: Placeholder polyfill.
 			$('form').placeholder();
 
+<<<<<<< HEAD
 		// Prioritize "important" elements on medium.
 			skel.on('+medium -medium', function() {
 				$.prioritize(
@@ -80,32 +106,77 @@
 				$.scrollzer(ids, { pad: 300, lastHack: true });
 
 		// Off-Canvas Navigation.
+=======
+		// Prioritize "important" elements on mobile.
+			skel.on('+mobile -mobile', function() {
+				$.prioritize(
+					'.important\\28 mobile\\29',
+					skel.breakpoint('mobile').active
+				);
+			});
+
+		// Off-Canvas Sidebar.
+
+			// Height hack.
+				var $sc = $('#sidebar, #content'), tid;
+
+				$window
+					.on('resize', function() {
+						window.clearTimeout(tid);
+						tid = window.setTimeout(function() {
+							$sc.css('min-height', $document.height());
+						}, 100);
+					})
+					.on('load', function() {
+						$window.trigger('resize');
+					})
+					.trigger('resize');
+>>>>>>> parent of 34e9465... tiral4
 
 			// Title Bar.
 				$(
 					'<div id="titleBar">' +
+<<<<<<< HEAD
 						'<a href="#header" class="toggle"></a>' +
+=======
+						'<a href="#sidebar" class="toggle"></a>' +
+>>>>>>> parent of 34e9465... tiral4
 						'<span class="title">' + $('#logo').html() + '</span>' +
 					'</div>'
 				)
 					.appendTo($body);
 
+<<<<<<< HEAD
 			// Header.
 				$('#header')
+=======
+			// Sidebar
+				$('#sidebar')
+>>>>>>> parent of 34e9465... tiral4
 					.panel({
 						delay: 500,
 						hideOnClick: true,
 						hideOnSwipe: true,
 						resetScroll: true,
 						resetForms: true,
+<<<<<<< HEAD
 						side: 'right',
 						target: $body,
 						visibleClass: 'header-visible'
+=======
+						side: 'left',
+						target: $body,
+						visibleClass: 'sidebar-visible'
+>>>>>>> parent of 34e9465... tiral4
 					});
 
 			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
 				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+<<<<<<< HEAD
 					$('#titleBar, #header, #wrapper')
+=======
+					$('#titleBar, #sidebar, #main')
+>>>>>>> parent of 34e9465... tiral4
 						.css('transition', 'none');
 
 	});
